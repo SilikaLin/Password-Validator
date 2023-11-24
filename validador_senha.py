98f38161c95abc791791af48e409f4a68ddbe740
Luana Oliveira <79430579+SilikaLin@users.noreply.github.com>
2023-11-24 13:33:18 -0300

    implementação validador_senha.py

--- validador_senha.py
+++ validador_senha.py
@@ -1,22 +1,19 @@
 class ValidadorSenha:
 
-    def validar_senha(self, senha):
-        if len(senha) < 8:
-            return False
+def validar_senha(senha):
+    if len(senha) < 8:
+        return False, "A senha deve ter no mínimo 8 caracteres"
 
-        tem_maiuscula = False
-        tem_minuscula = False
-        tem_digito = False
-        tem_caracter_especial = False
+    tem_numero = any(char.isdigit() for char in senha)
+    if not tem_numero:
+        return False, "A senha deve conter pelo menos um número"
 
-        for caractere in senha:
-            if caractere.isupper():
-                tem_maiuscula = True
-            elif caractere.islower():
-                tem_minuscula = True
-            elif caractere.isdigit():
-                tem_digito = True
-            else:
-                tem_caracter_especial = True
+    tem_maiuscula = any(char.isupper() for char in senha)
+    if not tem_maiuscula:
+        return False, "A senha deve conter pelo menos uma letra maiúscula"
 
-        return tem_maiuscula and tem_minuscula and tem_digito and tem_caracter_especial
+    tem_minuscula = any(char.islower() for char in senha)
+    if not tem_minuscula:
+        return False, "A senha deve conter pelo menos uma letra minúscula"
+
+    return True, "Senha válida"
